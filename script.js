93cd3bc9f4480eb39921a8fa75b0b3888bb87d9f
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2020-09-10 14:00:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-$('.signup-show').click(function() {
-  $('#signup-modal').fadeIn();
-});
-$('#close-modal').click(function() {
-  $('#signup-modal').fadeOut();
+$(function() {
+  $('#login-show').click(function(){
+    $('#login-modal').fadeIn();
+  });
+
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+
+  // 「.close-modal」要素にclickイベントを設定してください
+  $('.close-modal').click(function() {
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  })
+
+
 });
